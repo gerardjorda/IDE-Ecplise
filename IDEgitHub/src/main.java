1c046128dc78a1d7353ca9b622db3da23c5aa093
Gerard <Gerard@DESKTOP-UV586JK>
2019-10-21 18:50:10 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- IDEgitHub/src/main.java
+++ IDEgitHub/src/main.java
@@ -4,9 +4,6 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-		//aafsfadsfsgsagasdgasdfsadgsdgsagsadggsag
-		
-		//AAAAAAeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+		//COMENTARI DE LA BRANCA1
 	}	
-
 }
